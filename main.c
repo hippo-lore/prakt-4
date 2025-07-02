081a3fc1bf9ddd339ceac7590904853ac5541d77
hippo-lore <kn24-o.kovalchuk@nubip.edu.ua>
2025-07-02 12:16:31 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, count = 0;
+    
+    printf("Введіть натуральне число n (1 < n < 150): ");
+    scanf("%d", &n);
+
+    for (int m = 1; m <= n; m++) {
+        if (n % m == n / m) {
+            count++;
+        }
+    }
+
+    printf("Кількість рівних дільників: %d\n", count);
+    return 0;
+}
